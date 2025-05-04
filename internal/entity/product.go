8dc1c787377390bab3c6158a37217f34c7d356c6
bianor-junior <bianor.tech@gmail.com>
2025-05-04 18:09:03 -0300

    Adiciona implementação da estrutura Product e funções de validação e criação

---
+++ internal/entity/product.go
@@ -0,0 +1,54 @@
+package entity
+
+import (
+	"api-golang/pkg/entity"
+	"errors"
+	"time"
+)
+
+var (
+	ErrIDIsRequired = errors.New("ID is required")
+	ErrIvalidID = errors.New("ID is invalid")
+	ErrNameIsRequired = errors.New("name is required")
+	ErrPriceIsRequired = errors.New("price is required")
+	ErrIvalidPrice = errors.New("price is invalid")
+
+)
+
+type Product struct {
+	ID   entity.ID
+	Name string
+	Price int
+	CreatedAt string
+	UpdatedAt string
+	DeletedAt string
+}
+
+func (p *Product) Validate() error {
+	if p.ID.String() == "" {
+		return ErrIDIsRequired
+	}
+	if _, err := entity.ParseID(p.ID.String()); err != nil {
+		return ErrIvalidID
+	}
+	if p.Name == "" {
+		return ErrNameIsRequired
+	}
+	if p.Price <= 0 {
+		return ErrIvalidPrice
+	}
+	return nil
+}
+
+func NewProduct(id entity.ID, name string, price int) (*Product, error) {
+	product := &Product{
+		ID: id,
+		Name: name,
+		Price: price,
+		CreatedAt: time.Now().Format(time.RFC3339),
+	}
+	if err := product.Validate(); err != nil {
+		return nil, err
+	}
+	return product, nil
+}
