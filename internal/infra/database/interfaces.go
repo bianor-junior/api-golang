5b754f624581ea2dfb8a891d487541f4341ab89b
bianor-junior <bianor.tech@gmail.com>
2025-05-04 22:17:50 -0300

    Adiciona interface ProductInterface com métodos para criação, busca, atualização e deleção de produtos

--- internal/infra/database/interfaces.go
+++ internal/infra/database/interfaces.go
@@ -6,3 +6,11 @@ type UserInterface interface {
 	CreateUser(user *entity.User) (*entity.User, error)
 	FindUserByEmail(email string) (*entity.User, error)
 }
+
+type ProductInterface interface {
+	Create(product *entity.Product) error
+	FindAll(page, limit int, sort string) ([]entity.Product, error)
+	FindById(id string) (*entity.Product, error)
+	Update (product *entity.Product) (error)
+	Delete(id string) error
+}
